46c029ae3740302258f23f3d76ac3075d8587ac0
Lilia Manukyan <lilia.manukyan@hotmail.com>
2016-05-08 21:23:12 +0400

    decimal type

--- Projects/Projects/Program.cs
+++ Projects/Projects/Program.cs
@@ -1,24 +1,24 @@
-﻿using System;
+﻿public class Account {
 
-public class GradeBook {
- 
-    // is the same as the commented out one
-    public string CourseName { get; set; }
+    private decimal balance;
 
-    /* public string CourseName {
+    public Account(decimal initialBalance) {
+        Balance = initialBalance;    
+    }
+
+    public void Credit(decimal amount) {
+        Balance += amount;
+    }
+
+    public decimal Balance {
         get {
-            return courseName;
+            return balance;
         }
         set {
-            courseName = value;
+            if (value >= 0) {
+                balance = value;
+            }
         }
-    } */
-
-    public GradeBook(string name) {
-        CourseName = name; // the property CourseName is used here
     }
 
-    public void DisplayMessage() {
-        Console.WriteLine("Welcome to the Grade Book for {0}!", CourseName);
-    }
 }
