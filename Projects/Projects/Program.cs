5b65c8c59befbaa8c449853bdd5b3674746a507d
Lilia Manukyan <lilia.manukyan@hotmail.com>
2016-05-08 14:41:45 +0400

    Adding two integers

--- Projects/Projects/Program.cs
+++ Projects/Projects/Program.cs
@@ -2,6 +2,20 @@
 
 public class Program {
     public static void Main(string[] args) {
-        Console.WriteLine("Welcome to C# Programming!");
+
+        int number1;
+        int number2;
+        int sum;
+
+        Console.Write("Enter first int: ");
+        number1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second int: ");
+        number2 = Convert.ToInt32(Console.ReadLine());
+
+        sum = number1 + number2;
+
+        Console.WriteLine("Sum is {0}", sum);
+
     }
 }
