196a34a0287786bdfbe850861dc66b759857f77a
Lilia Manukyan <lilia.manukyan@hotmail.com>
2016-05-13 15:13:00 +0400

    foreach example

--- Projects/Projects/Program.cs
+++ Projects/Projects/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+
+public class Program {
+    public static void Main(string[] args) {
+
+        int[] array = { 87, 68, 94, 100, 83, 78, 85, 91, 76, 87 };
+        int total = 0;
+
+        foreach (int number in array) {
+            total += number;
+        }
+
+        Console.WriteLine("Total: {0}", total);
+
+    }
+}
