ea5930894a95d92108cb0d875771c308c6443106
Lilia Manukyan <lilia.manukyan@hotmail.com>
2016-05-19 16:52:45 +0400

    Time class: this

--- Projects/Projects/Program.cs
+++ Projects/Projects/Program.cs
@@ -6,26 +6,21 @@ public class Time {
     private int minute;
     private int second;
 
-    public void SetTime(int h, int m, int s) {
-        if ((h >= 0 && h < 24) && (m >= 0 && m < 60) &&
-            s >= 0 && s < 60) {
-            hour = h;
-            minute = m;
-            second = s;
-        } else {
-            throw new ArgumentOutOfRangeException();
-        }
+    public Time(int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public string BuildString() {
+        return string.Format("{0,24}: {1}\n{2,24}: {3}",
+            "this.ToUniversalString()", this.toUniversalString(),
+            "ToUniversalString()", toUniversalString());
     }
 
     public string toUniversalString() {
         return string.Format("{0:D2}:{1:D2}:{2:D2}",
-            hour, minute, second);
-    }
-
-    public override string ToString() {
-        return string.Format("{0}:{1:D2}:{2:D2} {3}",
-            ((hour == 0 || hour == 12) ? 12 : hour % 12),
-            minute, second, (hour < 12 ? "AM" : "PM"));
+            this.hour, this.minute, this.second);
     }
 
 }
--- Projects/Projects/Test.cs
+++ Projects/Projects/Test.cs
@@ -3,32 +3,8 @@
 public class Program {
     public static void Main(string[] args) {
 
-        Time time = new Time();
-
-        Console.Write("The initial universal time is: ");
-        Console.WriteLine(time.toUniversalString());
-        Console.Write("The initial standard time is: ");
-        Console.WriteLine(time.ToString());
-        Console.WriteLine();
-
-        time.SetTime(13, 27, 6);
-        Console.Write("Universal time after SetTime is: ");
-        Console.WriteLine(time.toUniversalString());
-        Console.Write("Standard time after SetTime is: ");
-        Console.WriteLine(time.ToString());
-        Console.WriteLine();
-
-        try {
-            time.SetTime(99, 99, 99);
-        }
-        catch(ArgumentOutOfRangeException exp) {
-            Console.WriteLine(exp.Message + "\n");
-        }
-
-        Console.WriteLine("After attempting invalid settings:");
-        Console.Write("Universal time: ");
-        Console.WriteLine(time.toUniversalString());
-        Console.Write("Standard time: ");
-        Console.WriteLine(time.ToString());
+        Time time = new Time(15, 30, 19);
+        Console.WriteLine(time.BuildString());
+     
     }
 }
