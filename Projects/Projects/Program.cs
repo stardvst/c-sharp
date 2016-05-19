65dd3e5555ed412996d3b8630d51a984bc4dd7b7
Lilia Manukyan <lilia.manukyan@hotmail.com>
2016-05-19 18:45:14 +0400

    Time class: overloaded constructors

--- Projects/Projects/Program.cs
+++ Projects/Projects/Program.cs
@@ -6,21 +6,71 @@ public class Time {
     private int minute;
     private int second;
 
-    public Time(int hour, int minute, int second) {
-        this.hour = hour;
-        this.minute = minute;
-        this.second = second;
+    public Time(int h = 0, int m = 0, int s = 0) {
+        SetTime(h, m, s);        
     }
 
-    public string BuildString() {
-        return string.Format("{0,24}: {1}\n{2,24}: {3}",
-            "this.ToUniversalString()", this.toUniversalString(),
-            "ToUniversalString()", toUniversalString());
+    public Time(Time time) 
+        :this(time.Hour, time.Minute, time.Second) {
+        // empty body
     }
 
-    public string toUniversalString() {
+    public void SetTime(int h, int m, int s) {
+        Hour = h;
+        Minute = minute;
+        Second = second;
+    }
+
+    public int Hour {
+        get {
+            return hour;
+        }
+        set {
+            if (value >= 0 && value < 24) {
+                hour = value;
+            } else {
+                throw new ArgumentOutOfRangeException("Hour",
+                    value, "Hour must be 0-23");    
+            }
+        }
+    }
+
+    public int Minute {
+        get {
+            return minute;
+        }
+        set {
+            if (value >= 0 && value < 60) {
+                minute = value;
+            } else {
+                throw new ArgumentOutOfRangeException("Minute",
+                    value, "minute must be 0-59");
+            }
+        }
+    }
+
+    public int Second {
+        get {
+            return second;
+        }
+        set {
+            if (value >=0 && value < 60) {
+                second = value;
+            } else {
+                throw new ArgumentOutOfRangeException("Second",
+                    value, "Second must be 0-59");
+            }
+        }
+    }
+
+    public string ToUniversalString() {
         return string.Format("{0:D2}:{1:D2}:{2:D2}",
-            this.hour, this.minute, this.second);
+            Hour, Minute, Second);
     }
 
+    public override string ToString() {
+        return string.Format("{0}:{1:D2}:{2:D2} {3}",
+            ((Hour == 0 || Hour == 12) ? 12 : Hour % 12),
+            Minute, Second, (Hour < 12 ? "AM" : "PM"));
+    }
 }
--- Projects/Projects/Test.cs
+++ Projects/Projects/Test.cs
@@ -3,8 +3,41 @@
 public class Program {
     public static void Main(string[] args) {
 
-        Time time = new Time(15, 30, 19);
-        Console.WriteLine(time.BuildString());
-     
+        Time t1 = new Time(); // 00:00:00
+        Time t2 = new Time(2); // 02:00:00
+        Time t3 = new Time(21, 34); // 21:34:00
+        Time t4 = new Time(12, 25, 42); // 12:25:42
+        Time t5 = new Time(t4); // 12:25:42
+        Time t6; // initialized later in the program
+
+        Console.WriteLine("Constructed with:\n");
+        Console.WriteLine("t1: all arguments defaulted");
+        Console.WriteLine(" {0}", t1.ToUniversalString()); // 00:00:00
+        Console.WriteLine(" {0}\n", t1.ToString()); // 12:00:00 AM
+
+        Console.WriteLine("t2: hour specified; minute and second defaulted");
+        Console.WriteLine(" {0}", t2.ToUniversalString()); // 02:00:00
+        Console.WriteLine(" {0}\n", t2.ToString()); // 2:00:00 AM
+
+        Console.WriteLine("t3: hour and minute specified; second defaulted");
+        Console.WriteLine(" {0}", t3.ToUniversalString()); // 21:34:00
+        Console.WriteLine(" {0}\n", t3.ToString()); // 9:34:00 PM
+
+        Console.WriteLine("t4: hour, minute and second specified");
+        Console.WriteLine(" {0}", t4.ToUniversalString()); // 12:25:42
+        Console.WriteLine(" {0}\n", t4.ToString()); // 12:25:42 PM
+
+        Console.WriteLine("t5: Time2 object t4 specified");
+        Console.WriteLine(" {0}", t5.ToUniversalString()); // 12:25:42
+        Console.WriteLine(" {0}", t5.ToString()); // 12:25:42 PM
+
+        try {
+            t6 = new Time(27, 74, 99);
+        }
+        catch(ArgumentOutOfRangeException ex) {
+            Console.WriteLine("\nException while initializing t6:");
+            Console.WriteLine(ex.Message);
+        }
+
     }
 }
