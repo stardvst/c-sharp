c30267c4ec091e97fc7c60f3c8d09e9c31664c45
Lilia Manukyan <lilia.manukyan@hotmail.com>
2016-05-20 09:12:52 +0400

    Composition demonstration

--- Projects/Projects/Program.cs
+++ Projects/Projects/Program.cs
@@ -1,76 +1,20 @@
-﻿using System;
+﻿public class Employee {
 
-public class Time {
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public Date BirthDate { get; private set; }
+    public Date HireDate { get; private set; }
 
-    private int hour;
-    private int minute;
-    private int second;
-
-    public Time(int h = 0, int m = 0, int s = 0) {
-        SetTime(h, m, s);        
-    }
-
-    public Time(Time time) 
-        :this(time.Hour, time.Minute, time.Second) {
-        // empty body
-    }
-
-    public void SetTime(int h, int m, int s) {
-        Hour = h;
-        Minute = minute;
-        Second = second;
-    }
-
-    public int Hour {
-        get {
-            return hour;
-        }
-        set {
-            if (value >= 0 && value < 24) {
-                hour = value;
-            } else {
-                throw new ArgumentOutOfRangeException("Hour",
-                    value, "Hour must be 0-23");    
-            }
-        }
-    }
-
-    public int Minute {
-        get {
-            return minute;
-        }
-        set {
-            if (value >= 0 && value < 60) {
-                minute = value;
-            } else {
-                throw new ArgumentOutOfRangeException("Minute",
-                    value, "minute must be 0-59");
-            }
-        }
-    }
-
-    public int Second {
-        get {
-            return second;
-        }
-        set {
-            if (value >=0 && value < 60) {
-                second = value;
-            } else {
-                throw new ArgumentOutOfRangeException("Second",
-                    value, "Second must be 0-59");
-            }
-        }
-    }
-
-    public string ToUniversalString() {
-        return string.Format("{0:D2}:{1:D2}:{2:D2}",
-            Hour, Minute, Second);
+    public Employee(string first, string last, Date birth, Date hire) {
+        FirstName = first;
+        LastName = last;
+        BirthDate = birth;
+        HireDate = hire;
     }
 
     public override string ToString() {
-        return string.Format("{0}:{1:D2}:{2:D2} {3}",
-            ((Hour == 0 || Hour == 12) ? 12 : Hour % 12),
-            Minute, Second, (Hour < 12 ? "AM" : "PM"));
+        return string.Format("{0}, {1} Hired: {2} Birthday: {3}",
+            FirstName, LastName, HireDate, BirthDate);
     }
+
 }
