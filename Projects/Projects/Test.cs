24d43f3d2b88f2a8d94f62e60f7d46cdb193119c
Lilia Manukyan <lilia.manukyan@hotmail.com>
2016-05-20 17:40:21 +0400

    Duplicate Word Removal

--- Projects/Projects/Test.cs
+++ Projects/Projects/Test.cs
@@ -1,23 +1,20 @@
 ﻿using System;
+using System.Linq;
 
 public class Program {
     public static void Main(string[] args) {
 
-        Console.WriteLine("Employees before instantiation: {0}",
-            Employee.Count);
+        Console.Write("Enter a senctence: ");
+        string input = Console.ReadLine();
 
-        Employee e1 = new Employee("Susan", "Baker");
-        Employee e2 = new Employee("Bob", "Blue");
-
-        Console.WriteLine("\nEmployees after instantiation: {0}",
-            Employee.Count);
-
-        Console.WriteLine("\nEmployee 1: {0} {1}\nEmployee 2: {2} {3}\n",
-            e1.FirstName, e1.LastName,
-            e2.FirstName, e2.LastName);
-
-        e1 = null; // mark object referenced by e1 as no longer needed
-        e2 = null; // mark object referenced by e2 as no longer needed
+        var orderedWords =
+            from word in input.Split()
+            orderby word.ToLower()
+            select word.ToLower();
+        
+        foreach (var word in orderedWords.Distinct()) {
+            Console.Write("{0} ", word);
+        }   
 
     }
 }
