9d69a4465977e913f538d85f833dd6671d7e1a0c
Lilia Manukyan <lilia.manukyan@hotmail.com>
2016-05-22 12:01:53 +0400

    Demonstrating Polymorphism

--- Projects/Projects/Test.cs
+++ Projects/Projects/Test.cs
@@ -3,23 +3,33 @@
 public class Program {
     public static void Main(string[] args) {
 
+        CommissionEmployee commissionEmployee = new CommissionEmployee(
+            "Sue", "Jones", "222-22-2222", 10000.00M, .06M);
+
         BasePlusCommissionEmployee basePlusCommissionEmployee =
             new BasePlusCommissionEmployee("Bob", "Lewis",
             "333-33-3333", 5000.00M, .04M, 300.00M);
 
-        Console.WriteLine("Employee information obtained by properties and methods: \n");
-        Console.WriteLine("First name is {0}", basePlusCommissionEmployee.FirstName);
-        Console.WriteLine("Last name is {0}", basePlusCommissionEmployee.LastName);
-        Console.WriteLine("Social security number is {0}", basePlusCommissionEmployee.SocialSecurityNumber);
-        Console.WriteLine("Gross sales are {0:C}", basePlusCommissionEmployee.GrossSales);
-        Console.WriteLine("Commission rate is {0:F2}", basePlusCommissionEmployee.CommissionRate);
-        Console.WriteLine("Earnings are {0:C}", basePlusCommissionEmployee.Earnings());
+        Console.WriteLine("{0} {1}:\n\n{2}\n{3}: {4:C}\n",
+            "Call CommissionEmployee's ToString and Earnings methods ",
+            "with base-class reference to base class object",
+            commissionEmployee.ToString(),
+            "earnings", commissionEmployee.Earnings());
 
-        basePlusCommissionEmployee.BaseSalary = 1000.00M; // set base salary
+        Console.WriteLine("{0} {1}:\n\n{2}\n{3}: {4:C}\n",
+            "Call BasePlusCommissionEmployee's ToString and Earnings ",
+               "methods with derived class reference to derived-class object",
+            basePlusCommissionEmployee.ToString(),
+               "earnings", basePlusCommissionEmployee.Earnings());
 
-        Console.WriteLine("\n{0}:\n\n{1}",
-            "Updated employee information obtained by ToString", basePlusCommissionEmployee);
-        Console.WriteLine("earnings: {0:C}", basePlusCommissionEmployee.Earnings());
+        // invoke ToString and Earnings on derived-class object
+        // using base-class variable
+        CommissionEmployee commissionEmployee2 = basePlusCommissionEmployee;
+        Console.WriteLine("{0} {1}:\n\n{2}\n{3}: {4:C}",
+            "Call BasePlusCommissionEmployee's ToString and Earnings ",
+            "with base class reference to derived-class object",
+            commissionEmployee2.ToString(), "earnings",
+            commissionEmployee2.Earnings());
 
     }
 }
